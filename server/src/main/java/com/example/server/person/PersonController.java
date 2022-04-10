71b04d5fa15e3cf361d3629c43fc5cd31a026651
paminna <ribekks@gmail.com>
2022-04-10 21:11:33 +0300

    корректировки

--- server/src/main/java/com/example/server/person/PersonController.java
+++ server/src/main/java/com/example/server/person/PersonController.java
@@ -1,10 +1,12 @@
 package com.example.server.person;
 
+import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Log
 @RestController
 @RequestMapping("/server")
 public class PersonController {
@@ -14,10 +16,10 @@ public class PersonController {
     @PostMapping("/get-person-information")
     public PersonDto getPersonById(@RequestBody PersonRequest request)
     {
-        PersonDto personDto = personService.findPersonByPersonId(request.getId()).get();
+        PersonDto personDto = personService.findPersonByPersonId(request.getId()).isPresent() ? personService.findPersonByPersonId(request.getId()).get() : null;
         if (personDto != null)
             return personDto;
-//        log.info(no person found by id 131112)
+        log.info("Couldn't find person by id" + request.getId());
         return null;
     }
 
@@ -26,7 +28,7 @@ public class PersonController {
     {
         if (personService.findAll().size() != 0)
             return personService.findAllPeople();
+        log.info("There is no accounts yet");
         return null;
-        //log.info("there is no people
     }
 }
