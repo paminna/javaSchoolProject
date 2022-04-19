7e9dce65c223b3000b73385dad217465efe928bb
paminna <ribekks@gmail.com>
2022-04-19 13:26:37 +0300

    auth

--- client/src/main/java/com/example/client/client/controller/ClientController.java
+++ client/src/main/java/com/example/client/client/controller/ClientController.java
@@ -4,8 +4,15 @@ import com.example.client.client.ClientRequest;
 import com.example.server.controller.PersonController;
 import com.example.server.dto.AccountDto;
 import com.example.server.security.SecurityConfig;
+import com.example.server.security.UserDetailsServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,6 +22,9 @@ import java.util.List;
 @RestController
 @RequestMapping("/client")
 public class ClientController {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Value("http://localhost:8083")
     private String url;
@@ -34,6 +44,31 @@ public class ClientController {
                 restTemplate.postForObject(url + "/account/get-accounts-by", request, AccountDto[].class);
         List<AccountDto> list = Arrays.asList(responses);
         return list;
+    }
+
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/auth/{login}/{password}")
+    public String auth(@PathVariable("login") String login, @PathVariable("password") String password) throws Exception {
+        try {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(login);
+
+            if(passwordEncoder.matches(password, userDetails.getPassword())) {
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(login, null, userDetails.getAuthorities());
+                authentication.setDetails(userDetails);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+            else {
+                return "PASSWORD_NOT_MATCHES";
+            }
+        }
+        catch (UsernameNotFoundException ex) {
+            return "USER_NOT_FOUND";
+        }
+
+        return "OK";
     }
 
 }
